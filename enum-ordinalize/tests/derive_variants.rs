155fd9c8e4eff2083a08e0fc928a6aabebad8b0f
Magic Len <len@magiclen.org>
2023-11-22 12:28:28 +0800

    improve tests

--- enum-ordinalize/tests/derive_variants.rs
+++ enum-ordinalize/tests/derive_variants.rs
@@ -3,7 +3,7 @@
 use enum_ordinalize::Ordinalize;
 
 #[test]
-fn get_variants_const_fn() {
+fn get_variants_const_fn_1() {
     #[derive(Debug, PartialEq, Eq, Ordinalize)]
     #[ordinalize(variants(pub const fn get_variants, doc = "Retrieve the array of `MyEnum`'s variants."))]
     enum MyEnum {
@@ -16,9 +16,35 @@ fn get_variants_const_fn() {
 }
 
 #[test]
-fn get_variants_const() {
+fn get_variants_const_fn_2() {
+    #[derive(Debug, PartialEq, Eq, Ordinalize)]
+    #[ordinalize(variants(pub fn get_variants, doc = "Retrieve the array of `MyEnum`'s variants."))]
+    enum MyEnum {
+        A,
+        B,
+        C,
+    }
+
+    assert_eq!([MyEnum::A, MyEnum::B, MyEnum::C], MyEnum::get_variants());
+}
+
+#[test]
+fn get_variants_const_1() {
     #[derive(Debug, PartialEq, Eq, Ordinalize)]
     #[ordinalize(variants(pub const VARIANTS, doc = "The array of `MyEnum`'s variants."))]
+    enum MyEnum {
+        A,
+        B,
+        C,
+    }
+
+    assert_eq!([MyEnum::A, MyEnum::B, MyEnum::C], MyEnum::VARIANTS);
+}
+
+#[test]
+fn get_variants_const_2() {
+    #[derive(Debug, PartialEq, Eq, Ordinalize)]
+    #[ordinalize(variants(pub VARIANTS, doc = "The array of `MyEnum`'s variants."))]
     enum MyEnum {
         A,
         B,
